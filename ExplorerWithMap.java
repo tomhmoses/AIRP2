6097a2e8bdb61c9bd822ba44fe89efd8cfe51a77
Tom Moses <tom@tmoses.co.uk>
2019-03-05 12:36:42 +0000

    added description

--- ExplorerWithMap.java
+++ ExplorerWithMap.java
@@ -1,3 +1,9 @@
+/**
+ * <img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/2BAZlr8/young-lost-man-with-a-map-looking-for-direction-in-forest_ek79a5x0__F0011.png" width="250">
+ * 
+ * @author Tom
+ *
+ */
 public class ExplorerWithMap extends Explorer {
     
 	public ExplorerWithMap(int x, int y, Maze maze)
