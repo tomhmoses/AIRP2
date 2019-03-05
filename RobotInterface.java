0c2ca4eb24d25d02b2951bf267ad59c8c707ca07
Tom Moses <tom@tmoses.co.uk>
2019-03-05 12:22:27 +0000

    added descriptions

--- RobotInterface.java
+++ RobotInterface.java
@@ -1,17 +1,46 @@
 
+/**
+ * Juan is a robot
+ * 
+ * <img src="https://i.imgur.com/GMByDzn.png">
+ * 
+ * @author <b style="color:blue;background-color:pink;">Tom</b>
+ */
 public interface RobotInterface
 {
 
+	/**
+	 * The robot will move one tile north
+	 */
 	void MoveNorth();
 
+	/**
+	 * The robot will move one tile South 
+	 */
 	void MoveSouth();
 
+	/**
+	 * The robot will move one tile East
+	 */
 	void MoveEast();
 
+	/**
+	 * The robot will move one tile West
+	 */
 	void MoveWest();
 
+	/**
+	 * @return if it has reached the goal
+	 * 
+	 * <p>i.e. using the <code>ColorSensor</code> to see if it is on a <b style="color:red;">red</b> tile.</p>
+	 */
 	Boolean reachedGoal();
 
+	/**
+	 * @return the current walls in a boolean array in order <code>[N,E,S,W]</code>.
+	 * 
+	 * <p>It can and should return <code>null</code> for the wall behind the robot.</p>
+	 */
 	Boolean[] getCurrentWalls();
 
 }
