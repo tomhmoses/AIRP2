5de23646b22a1cd0c4230e6fd0ca6f9a599ddede
jnk838 <43723785+jnk838@users.noreply.github.com>
2019-03-08 15:17:19 +0000

    Update RobotMainTest.java

--- RobotMainTest.java
+++ RobotMainTest.java
@@ -4,22 +4,22 @@ public class RobotMainTest
 	{
 		Juan juan = new Juan();
 		Boolean[] walls = juan.getCurrentWalls();
-		jusn.LCDOut(walls.toString(), 0);
+		juan.LCDOut(walls.toString(), 0);
 		
 		juan.MoveNorth();
 		walls = juan.getCurrentWalls();
-		jusn.LCDOut(walls.toString(), 1);
+		juan.LCDOut(walls.toString(), 1);
 
 		juan.MoveEast();
 		walls = juan.getCurrentWalls();
-		jusn.LCDOut(walls.toString(), 2);
+		juan.LCDOut(walls.toString(), 2);
 		
 		juan.MoveWest();
 		walls = juan.getCurrentWalls();
-		jusn.LCDOut(walls.toString(), 3);
+		juan.LCDOut(walls.toString(), 3);
 		
 		juan.MoveSouth();
 		walls = juan.getCurrentWalls();
-		jusn.LCDOut(walls.toString(), 4);
+		juan.LCDOut(walls.toString(), 4);
 	}	
 }
