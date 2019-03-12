a6541062f48accc221d5e197eb78e04d8da28ada
Tom Moses <tom@tmoses.co.uk>
2019-03-12 10:05:42 +0000

    now all in an if statement
    to easily disable it

--- EV3Server.java
+++ EV3Server.java
@@ -10,23 +10,26 @@ public class EV3Server
    
     public EV3Server()
     {
-        ServerSocket server;
-		try
-		{
-			server = new ServerSocket(port);
-	        LCD.drawString("Awaiting client..", 0, 5);
-	        Socket client = server.accept();
-	        LCD.clear();
-	        LCD.drawString("CONNECTED", 0, 5);
-	        OutputStream out = client.getOutputStream();
-	        oOut = new ObjectOutputStream(out);
-	        // TODO: what was this dOutflush thing?
-		}
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	if (true)
+    	{
+	        ServerSocket server;
+			try
+			{
+				server = new ServerSocket(port);
+		        LCD.drawString("Awaiting client..", 0, 5);
+		        Socket client = server.accept();
+		        LCD.clear();
+		        LCD.drawString("CONNECTED", 0, 5);
+		        OutputStream out = client.getOutputStream();
+		        oOut = new ObjectOutputStream(out);
+		        // TODO: what was this dOutflush thing?
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
     }
     
     public void send(Object obj) throws IOException
