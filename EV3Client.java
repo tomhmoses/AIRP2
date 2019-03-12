7e273cd94975838a9a490553188391a37b240786
Tom Moses <tom@tmoses.co.uk>
2019-03-12 10:05:05 +0000

    can now close

--- EV3Client.java
+++ EV3Client.java
@@ -4,6 +4,7 @@ import java.net.*;
 public class EV3Client
 {
 	ObjectInputStream oIn;
+	Socket sock;
 	
     public EV3Client() {
     	
@@ -11,7 +12,7 @@ public class EV3Client
 		{
         	System.out.println("waiting for connection");
             String ip = "10.0.1.1"; // BT
-            Socket sock = new Socket(ip, EV3Server.port);
+            sock = new Socket(ip, EV3Server.port);
             System.out.println("Connected");
             InputStream in = sock.getInputStream();
 			oIn = new ObjectInputStream(in);
@@ -27,5 +28,18 @@ public class EV3Client
     public Maze getMaze() throws ClassNotFoundException, IOException {
     	Maze obj = (Maze) oIn.readObject();
         return obj;
+    }
+    
+    public void close()
+    {
+    	try
+		{
+			sock.close();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
